175a18d070c6123c9ba5f324a5d5afebc242cbee
Cr7stian8 <cristianotropeiro@gmail.com>
2022-10-16 16:56:17 -0300

    Codigo fatorado

--- Interface01/modelo2/modelo2.js
+++ Interface01/modelo2/modelo2.js
@@ -1,12 +1,12 @@
 function comprar(){
     let tamanho = document.getElementById("tamanho");
     if(tamanho.value == "p"){
-        window.location.href = "https://mpago.la/117TkGA";
+        window.location.href = "https://mpago.la/1JgcbGX";
     }
     else if(tamanho.value == "m"){
-        window.location.href = "https://mpago.la/2yGXVzJ";
+        window.location.href = "https://mpago.la/28Rc2pc";
     }
     else{
-        window.location.href = "https://mpago.la/2PbrJQo";
+        window.location.href = "https://mpago.la/28Rc2pc";
     }
 }
---
+++ Interface01/modelo3/modelo3.js
@@ -0,0 +1,12 @@
+function comprar(){
+    let tamanho = document.getElementById("tamanho");
+    if(tamanho.value == "p"){
+        window.location.href = "https://mpago.la/2F1sdiC";
+    }
+    else if(tamanho.value == "m"){
+        window.location.href = "https://mpago.la/2YAsdeQ";
+    }
+    else{
+        window.location.href = "https://mpago.la/1m7T2TM";
+    }
+}
